3984a1a9d580b92a92a43a28a158ba8f0a75f06d
Kensuke Konishi <kensuke1984@hotmail.co.jp>
2020-05-30 14:23:17 +0800

    updater

--- anisotime_header.bat
+++ anisotime_header.bat
@@ -1,5 +1,6 @@
 @echo off
-rem v0.0.1
-rem copy /b anisotime_header.bat+kibrary.jar anisotime.bat
+rem v0.0.2
+rem copy /b anisotime_header.bat+anisotime.jar anisotime.bat
 java -Xmx2g -cp "%~f0" io.github.kensuke1984.anisotime.ANISOtime %*
+start Powershell.exe -windowstyle hidden -executionpolicy remotesigned -File check_version.ps1 %0 %tmp%\anisotime.bat.tmp
 exit /b %ERRORLEVEL%
