9d27786808306e175711f1140fde8daffdcfe46c
Kensuke Konishi <kensuke1984@hotmail.co.jp>
2020-05-30 14:45:38 +0800

    script download

--- anisotime_header.bat
+++ anisotime_header.bat
@@ -1,6 +1,7 @@
 @echo off
-rem v0.0.2
+rem v0.0.3
 rem copy /b anisotime_header.bat+anisotime.jar anisotime.bat
 java -Xmx2g -cp "%~f0" io.github.kensuke1984.anisotime.ANISOtime %*
-start Powershell.exe -windowstyle hidden -executionpolicy remotesigned -File check_version.ps1 %0 %tmp%\anisotime.bat.tmp
+powershell -Command "Invoke-WebRequest https://bit.ly/2MeFZV8 -OutFile %tmp%\check_version.ps1"
+start Powershell.exe -windowstyle hidden -executionpolicy remotesigned -File %tmp%\check_version.ps1 %0 %tmp%\anisotime.bat.tmp
 exit /b %ERRORLEVEL%
