a0285964d469556d0abe62b633066aa0c5d979f6
Kensuke Konishi <kensuke1984@hotmail.co.jp>
2020-08-01 16:39:53 +0800

    update system

--- anisotime_header.bat
+++ anisotime_header.bat
@@ -1,7 +1,11 @@
 @echo off
-rem v0.0.3
+rem v0.0.4
 rem copy /b anisotime_header.bat+anisotime.jar anisotime.bat
 java -Xmx2g -cp "%~f0" io.github.kensuke1984.anisotime.ANISOtime %*
-powershell -Command "Invoke-WebRequest https://bit.ly/2MeFZV8 -OutFile %tmp%\check_version.ps1"
-start Powershell.exe -windowstyle hidden -executionpolicy remotesigned -File %tmp%\check_version.ps1 %0 %tmp%\anisotime.bat.tmp
+IF /I "%ERRORLEVEL%" EQU "55" (
+    del %~df0
+    ren %~dp0\latest_anisotime %~nx0
+    echo ANISOtime is updated.
+    exit /b 55
+)
 exit /b %ERRORLEVEL%
