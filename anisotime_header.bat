66e8c45d5c71750c0aba6a9fb15fef334a7ef943
kensuke <kensuke1984@hotmail.co.jp>
2021-09-14 23:14:50 +0900

    header

--- anisotime_header.bat
+++ anisotime_header.bat
@@ -1,11 +1,11 @@
 @echo off
-rem v0.0.4
+rem v0.0.5
 rem copy /b anisotime_header.bat+anisotime.jar anisotime.bat
 java -Xmx2g -cp "%~f0" io.github.kensuke1984.anisotime.ANISOtime %*
 IF /I "%ERRORLEVEL%" EQU "55" (
     del %~df0
     ren %~dp0\latest_anisotime %~nx0
-    echo ANISOtime is updated.
+    start java -Xmx2g -cp "%~f0" io.github.kensuke1984.anisotime.ANISOtime %*
     exit /b 55
 )
 exit /b %ERRORLEVEL%
